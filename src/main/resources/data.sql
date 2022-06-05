704deabfac3aa82c9de0b08d4336317e61401289
JulianPetrov <j.petrov@blubito.com>
2022-06-05 14:29:34 +0300

    Add data.sql to initially fill db

---
+++ src/main/resources/data.sql
@@ -0,0 +1,19 @@
+INSERT INTO t_authority (name)
+VALUES ('ROLE_USER'),
+       ('ROLE_ADMIN');
+
+INSERT INTO t_user (id, email, first_name, last_name, password_hash, username)
+VALUES ((select nextval('sequence_generator')), 'julian@test.com', 'Julian', 'Petrov',
+        'julian',
+        'julian'),
+       ((select nextval('sequence_generator')), 'test@test.com', 'test', 'test',
+        '$2a$10$2O7fgfzE.UH9EZWk/1Zad.4xVkIoYCZ6XzY53rGDl.Be/KHWndN6K', 'test'),
+       ((select nextval('sequence_generator')), 'noadmin@test.com', 'noadmin', 'noadmin',
+        '$2a$10$Drjs8h.ZQqF3nsBu7VNuAey8gRu1n.7uuSmB3DqJnMBfPFq7N04Ni',
+        'noadmin');
+
+INSERT INTO t_user_authority(user_id, authority_name)
+VALUES ((select id from t_user where email = 'julian@test.com'), 'ROLE_USER'),
+       ((select id from t_user where email = 'test@test.com'), 'ROLE_USER'),
+       ((select id from t_user where email = 'test@test.com'), 'ROLE_ADMIN');
+
